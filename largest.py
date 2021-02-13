d8d77c97d2375d6c4e18410b75e71a88b0a2bf13
aishwarya324 <78892097+aishwarya324@users.noreply.github.com>
2021-02-13 11:51:36 +0530

    Add files via upload

---
+++ largest.py
@@ -0,0 +1,24 @@
+"largest"
+n1=float(input("Enter the first number: "));
+n2=float(input("Enter the second number: "));
+n3=float(input("Enter the Third number: "));
+def largest():      #function definition
+     if(n1>=n2) and (n1>=n2):
+         largest=n1
+     elif(n2>=n1) and (n2>=n3):
+         largest=num2
+     else:
+         largest=n3
+         print("Largest number is",largest)
+largest()
+pass
+
+"sum of digits"
+n1=float(input("Enter the first number: "));
+n2=float(input("Enter the second number: "));
+n3=float(input("Enter the Third number: "));
+def sum(a,b,c):
+    sum=a+b+c
+    return sum
+print("the sum of three numbers is:",sum(n1,n2,n3))
+
