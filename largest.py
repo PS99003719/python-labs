3ed270452516cc645348ccf22cb5519ee7008d74
aishwarya324 <78892097+aishwarya324@users.noreply.github.com>
2021-02-15 09:27:44 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -408,26 +408,10 @@ def fun(ipt):
                  n //= 1000
          for y in lst:
              i+= str(y) + "."
-         return n[::-1]#malformed time string //
-def m_t(t_s):
-    st = ""
-    l1 = t_s.split(":")
-    l2 = []
-    for i in l1:
-        l2.append(int(i))
-    
-    if l2[2] >= 60:
-        l2[1] += l2[2] // 60
-        l2[2] %= 60
-    if l2[1] >= 60:
-        l2[0] += l2[1] // 60
-        l2[1] %= 60
-    if l2[0] > 23:
-        return None
-    
-    for i in l2:
-        st += str(i) + ":"
-    return st[:-1]
+         return n[::-1] 
+
+
+
 
 #malformed time string //
 def m_t(t_s):
@@ -472,6 +456,65 @@ def m_d(d_s):
     return a[:-1]
 
 
+#mexican wave //
+def mex(s):
+    l = []
+    for i in range(len(s)):
+          a=s[:i].lower() + st[i].upper() + s[i+1:].lower()
+          l.append(a)
+    return l  
+
+#largest number by deleting the single digit //
+
+def m_delete(n):
+    l = []
+    m = str(n)
+    for i in m:
+        l.append(int(m.replace(i, '')))
+
+#accumulated strings //
+def acc_string(s):
+    a = ""
+    m= 0
+    for i in s:
+        a += i.upper() + i.lower()*m
+        a += "-"
+        m +=1 
+    return a[:-1]
+    return max(l) 
+
+
+
+#RGB TO HEX //
+def rth(value):
+    n1 = {10:"A", 11:"B", 12:"C", 13:"D", 14:"E", 15:"F"}
+    x = "0X"
+    flag = 0
+    for y in value:
+        if y > 255:
+            return None
+        elif y == 0:
+            x += "00"
+
+        while y>0 and y < 256:
+            if y < 16 and flag == 0:
+                r = y%16
+                if r>= 10:
+                    x += "0" + n1[r]
+                else:
+                    x+=  "0" + str(r)
+                y //= 16
+            elif y< 256:
+                flag = 1
+                r= y%16
+                if r >= 10:
+                    x += n1[r]
+                else:
+                    x+= str(r)
+                y //= 16
+            
+    return x
+    
 
 
 
